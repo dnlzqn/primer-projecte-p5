531909ed5e5e6ff1c146ebdd197dfa5f3b932463
danieluzquiano <55982609+danieluzquiano@users.noreply.github.com>
2019-10-03 10:38:26 +0200

    Add files via upload
    S'ha afegit la llibreria p5.sound i canviat l'arxiu sketch per incorporar getLevel()

--- sketch.js
+++ sketch.js
@@ -1,21 +1,28 @@
 let w = 400;
 let h = 600;
 
+let mic;
+
 function setup(){
-    //createCanvas(400,400); 
-    //createCanvas(w,h);
     createCanvas(windowWidth, windowHeight);
     background('red');
+
+    
 }
 
 function draw(){
-    background(150);
+
+    micLevel = mic.getLevel();
+    mov = map(micLevel, 0, 1, 10, 300);
+
+    //background(150);
     strokeWeight(40);
     stroke('blue');
     //noStroke();
     fill('yellow');
     //rect(0,0,100,100);
-    ellipse(width/2,height/2,100);
+    strokeWeight(1);
+    ellipse(width/2,height/2,100+mov);
     strokeWeight(1);
     noFill();
     ellipse(mouseX,mouseY,100);
@@ -26,7 +33,11 @@ function draw(){
     triangle(mouseX,mouseY,300,300,100,300);
     fill('brown');
     //rectMode(CENTER);
-    rect(0,0,200,200);
+    rect(0+mov*2.5,0+mov*2.5,200,200);
+}
+
+function touchStarted() {
+    getAudioContext().resume();
 }
 
 function windowResized(){
